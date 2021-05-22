cf20e650eafaeea8fce4e0cec470256471bd83ec
Ênio <enio.abrantes.silva@gmail.com>
2021-05-22 19:14:58 -0300

    Adding graphQL data for sectionTech

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -32,11 +32,23 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      title
+      techIcons {
+        title
+        icon {
+          url
+        }
+      }
+    }
+  }
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
       ...header
       ...sectionAboutProject
+      ...sectionTech
     }
   }
 `
